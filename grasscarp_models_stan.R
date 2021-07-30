b2ed54e654abfe0f1f3d926d7646f2d18cfba55a
danStich <36766897+danStich@users.noreply.github.com>
2021-07-30 09:44:27 -0400

    Update grasscarp_models_stan.R

--- grasscarp_models_stan.R
+++ grasscarp_models_stan.R
@@ -8,7 +8,6 @@
 # . Set options ----
   options(mc.cores = parallel::detectCores())  
   rstan_options(auto_write = TRUE)
-  Sys.setenv(LOCAL_CPPFLAGS = '-march=native')
 
 # . Function definition ----
 # ses
